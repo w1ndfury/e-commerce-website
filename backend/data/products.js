8bb7ffbcdc806be1d9d7b6e95de272fb45963f19
w1ndfury <w1ndfury216@gmail.com>
2021-03-08 19:42:40 +0200

    Mongo Schemas & Import Data Script

---
+++ backend/data/products.js
@@ -0,0 +1,11 @@
+const products = [
+    {
+        name: "Item 1",
+        description: "This is the description",
+        price: 99.99,
+        countInStock: 10,
+        imageUrl: "image_url"
+    }
+]
+
+module.exports = products;
